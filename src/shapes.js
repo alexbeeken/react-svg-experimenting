742a65df1dbc0251fe326d647679a51035e7a9a0
Alex Beeken <alexbeeken@Alexs-MacBook-Pro.local>
2017-04-23 19:14:19 -0700

    button to change color

--- src/shapes.js
+++ src/shapes.js
@@ -5,25 +5,29 @@ class Shapes extends Component {
   constructor() {
     super()
     this.state =
-      {
-        color: this.randomColor()
-      }
+      { color: this.randomColor() }
   }
 
   randomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
 
+  handleClick() {
+    this.setState(() => {
+      console.log('CALLED')
+      return { color: this.randomColor() }
+    })
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleClick.bind(this)}>change color</button>
         <svg version="1.1" width="250" height="250" xmlns="http://www.w3.org/2000/svg" >
-          <g>
-            <rect width="100%" height="100%" fill={this.state.color} />
-          </g>
+          <rect width="100%" height="100%" fill={this.state.color} />
         </svg>
       </div>
-    );
+    )
   }
 }
 
